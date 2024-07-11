322a8a0647a93164e8a22994cd1c5bf01ad1a02d
14321307 <gokulsabari007@gmail.com>
2024-07-11 10:48:32 +0530

    db/conn.js 10.47

--- Crud Task/server/db/conn.js
+++ Crud Task/server/db/conn.js
@@ -6,8 +6,7 @@ const pool=mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        port:process.env.DB_PORT
+        database: process.env.DB_DATABASE
        
     })
 
